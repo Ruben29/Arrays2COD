70d8c5bbb31d793dc93b55226e797fa0eb93ae24
rfernandezgonzalez <rfernandezgonzalez@eniac04.danielcastelao.org>
2015-01-19 09:27:37 +0100

    Inicio de ejercicio

---
+++ src/arrays2/Arrays2.java
@@ -0,0 +1,12 @@
+package arrays2;
+
+public class Arrays2 {
+
+    public static void main(String[] args) {
+        String[] nomeXog = {"Madinda", "Gorinda", "Burraca", "Mogogá", "Orongá"};
+        int[] dorsais = {5, 2, 9, 1, 7};
+        
+        Metodo.visualizar(nomeXog, dorsais);
+    }
+
+}
